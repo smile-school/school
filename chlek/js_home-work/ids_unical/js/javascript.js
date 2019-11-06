2baa83445d95b2eaaeb7a1e22eb203bae021c22a
YuriChlek <yurichlek@gmail.com>
2019-11-06 10:13:57 +0200

    Class work 5.11.

--- chlek/js_home-work/ids_unical/js/javascript.js
+++ chlek/js_home-work/ids_unical/js/javascript.js
@@ -1,4 +1,5 @@
 (function () {
+
     var ids = [
         [1, 2, 1, 3, 4, 3, 2, 3, 4, 5, 6, 7, 8, 6, 5, 3, 9, 45, 34, 34, 3, 10, 11, 12, 11, 14],
         [2, 3, 2, 4, 5, 4, 3, 4, 5, 6, 7, 8, 9, 7, 6, 4, 10, 46, 35, 35, 4, 11, 12, 13, 12, 15],
@@ -9,20 +10,72 @@
         [8, 9, 8, 10, 11, 10, 9, 10, 11, 12, 13, 14, 15, 13, 12, 10, 16, 52, 41, 41, 10, 17, 18, 19, 18, 21]
     ];
 
-    function getUniqueIds(arr) {
+    /*function getUniqueIds(arr) {
         var container = document.querySelector('.ids'),
             uniqueIds = [];
         for (var i = 0; i < arr.length; i++) {
             for (var j = 0; j < arr[i].length; j++) {
                 if (!uniqueIds.includes(arr[i][j])) {
                     uniqueIds.push(arr[i][j]);
-                    container.innerHTML += arr[i][j] + ',&nbsp;';
+                    /!*container.innerHTML += arr[i][j] + ',&nbsp;';*!/
                 }
             }
         }
-        console.log(uniqueIds);
+        return arr;
+       /!* console.log(uniqueIds);*!/
+    }*/
+
+    function getID(ides) {
+        var idArr = ides.toString().split(','),
+            retArr = [], tmpArr = [];
+        for (var i = 0; i < idArr.length; i++) {
+            if (tmpArr[idArr[i]]) continue;
+            tmpArr[idArr[i]] = 1;
+            retArr.push(+idArr[i]);
+        }
+        sortArr(retArr);
+        return retArr;
     }
 
-    getUniqueIds(ids);
+    function sortArr(arr) {
+        var flag = false, tmp = 0;
+        for (var j = 0; j < arr.length; j++) {
+            if (arr[j] > arr[j + 1]) {
+                tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+                flag = true;
+            }
+        }
+        if (flag) sortArr(arr);
+    }
 
+    var obj = {
+        firstName: 'Vasya',
+        lastName: 'Petrov',
+        age: 18,
+        education: ['hig', 'school', 'STEP'],
+        say: talk,
+    };
+
+    var obj2 = {
+        firstName: 'Petua',
+        lastName: 'Ivanov',
+        age: 18,
+        education: ['hig', 'school', 'STEP'],
+        say: talk,
+    };
+
+    function talk() {
+        console.log(this);
+        console.log(this.firstName + ' ' + this.lastName);
+    }
+
+    console.log(obj.say());
+
+    /* console.time('start');
+     getUniqueIds(ids);
+     console.timeEnd('start');*/
+    getID(ids);
+    console.log(getID(ids));
 })();
