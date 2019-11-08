26a422c7f4b5668de640cbadaaf1e8691506a4e4
YuriChlek <yurichlek@gmail.com>
2019-11-08 02:03:22 +0200

    Counter was finished

--- rozetka/js/counter.js
+++ rozetka/js/counter.js
@@ -3,37 +3,45 @@
     function elemObj() {
         var self = this;
         this.allShopping = document.querySelector('.shopping');
-        this.productBuy = document.createElement('span');
         this.countElement = document.createElement('span');
         this.buy = document.querySelectorAll('.buy');
         this.value = 0;
 
         function build(elem) {
             elem.forEach(function (item, elem) {
+                addElemProd(item);
                 click(item);
-
             });
+
             addElem(self.allShopping, self.countElement);
         }
 
         function click(element) {
             element.addEventListener('click', shoppingCounter);
+            element.addEventListener('click', clickCount(element));
         }
 
         function addElem(elem, addedElem) {
             elem.appendChild(addedElem);
-            return elem;
+        }
+
+        function clickCount(element) {
+            var counter = 0;
+            return function () {
+                counter++;
+                element.lastChild.innerHTML = counter;
+                if (!element.lastChild.classList.contains('active')) element.lastChild.classList.add('active');
+            }
         }
 
         function addElemProd(elem) {
             elem.innerHTML += '<span class=' + "counter-show" + '></span>';
+            return elem;
         }
 
         function shoppingCounter() {
             self.value++;
             addValue(self.allShopping, self.value, addClass(self.allShopping));
-            addClass(addElem(this, self.productBuy));
-            addElemProd(this);
         }
 
         function addValue(elem, val) {
