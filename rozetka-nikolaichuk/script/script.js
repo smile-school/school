8477fbeba693cf58d2536e04fb22641f7d6fa86b
NikOleks <nikolaychuk.alexander@gmail.com>
2019-11-21 03:12:28 +0200

    votes counter was created

--- rozetka-nikolaichuk/script/script.js
+++ rozetka-nikolaichuk/script/script.js
@@ -3,29 +3,28 @@
         pageOverlay = document.querySelector(".page-overlay"),
         formRespond = document.querySelector("#form-respond"),
         cancelRespond = document.querySelector(".cancelRespondBtn-js");
-    
+
     openFormBtn.addEventListener('click', function () {
         pageOverlay.classList.add("show");
     });
-    
-    cancelRespond.addEventListener("click",function () {
+
+    cancelRespond.addEventListener("click", function () {
         pageOverlay.classList.remove("show");
         clearFormFields(formRespond);
     });
-    
+
     formRespond.addEventListener("submit", function () {
         var data = new FormData(this);
         data.forEach(function (item, key) {
             console.log(key + " - " + item);
         })
     });
-    
+
     function clearFormFields(form) {
-        console.log(form.elements);
-        for (var i = 0; i < form.elements.length; i++){
-            if(form.elements[i].value){
+        for (var i = 0; i < form.elements.length; i++) {
+            if (form.elements[i].value) {
                 form.elements[i].value = "";
             }
         }
-    }
+    };
 })();
