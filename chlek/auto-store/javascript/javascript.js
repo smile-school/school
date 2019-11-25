5920280dcddd68a068d1a106f8b81079e217fdaf
YuriChlek <yurichlek@gmail.com>
2019-11-25 19:28:23 +0200

    Sliders was added.

---
+++ chlek/auto-store/javascript/javascript.js
@@ -0,0 +1,42 @@
+(function () {
+
+    var slider = new MySlider(
+        {
+            bigImageClass: '.show-img',
+            frameClass: '.frame',
+            clickElemClass: '.images-small',
+        }
+    );
+
+    $('.prod-cards').slick({
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<div class="prev"><img src="./img/prev.png" alt="prev"></div>',
+        nextArrow: '<div class="next"><img src="./img/next.png" alt="next"></div>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    autoplay: true,
+                    autoplaySpeed: 5000,
+                }
+            },
+            {
+                breakpoint: 520,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    autoplay: true,
+                    autoplaySpeed: 5000,
+                }
+            },
+        ]
+    });
+
+})();
---
+++ chlek/auto-store/javascript/slider.js
@@ -0,0 +1,35 @@
+(function () {
+    function MySlider(option) {
+        var self = this;
+        this.options = {
+            slideShow: document.querySelector(option.bigImageClass),
+            frame: document.querySelector(option.frameClass),
+            clickElement: document.querySelector(option.clickElemClass),
+        };
+
+        function build() {
+            self.options.clickElement.addEventListener('click', move);
+        }
+
+        function move(event) {
+            if (event.target.tagName === 'IMG') {
+                self.options.frame.style.left = event.target.offsetLeft + 'px';
+                show(event.target);
+            }
+        }
+
+        function show(elem) {
+            self.options.slideShow.classList.add('active');
+            setTimeout(function () {
+                self.options.slideShow.src = 'img/' + elem.dataset['image'];
+                self.options.slideShow.classList.remove('active');
+            }, 250);
+        }
+
+        build();
+
+    }
+
+    window.MySlider = MySlider;
+
+})();
