9b38b7a9b7ba9aea722b256248d39436fd6c2538
NikOleks <nikolaychuk.alexander@gmail.com>
2019-11-28 18:25:29 +0200

    form add respond

--- rozetka-nikolaichuk/script/respondCreator.js
+++ rozetka-nikolaichuk/script/respondCreator.js
@@ -1,82 +1,28 @@
 (function () {
-
-    /*var tree = [{
-        descriptor: "div",
-        classes: "product-responds__respond, respond-js",
-        children: [
-            {
-                descriptor: "div",
-                classes: "respond__heading, clearfix-block",
-                children: [
-                    {
-                        descriptor: "span",
-                        classes: "respond__heading__author",
-                        text: "Аноним Анонимович",
-                    },
-                    {
-                        descriptor: "span",
-                        classes: "respond__heading__trade-status",
-                    },
-                    {
-                        descriptor: "span",
-                        classes: "respond__heading__likes",
-                    },
-                    {
-                        descriptor: "span",
-                        classes: "respond__heading__date",
-                        text: "15 ноября 2019",
-                    }
-                ]
-            },
-            {
-                descriptor: "div",
-                classes: "respond__text",
-                children: [
-                    {
-                        descriptor: "p",
-                        text: "Lorem ipsum dolor sit amet, consectetur",
-                    },
-                    {
-                        descriptor: "p",
-                        text: "Lorem ipsum dolor sit amet",
-                        children:[
-                            {
-                                descriptor: "span",
-                                text: "Достоинства: ",
-                            },
-                        ]
-                    },
-                    {
-                        descriptor: "p",
-                        text: "reprehenderit sed, soluta tempora,",
-                        children: [
-                            {
-                                descriptor: "span",
-                                text: "Недостатки: ",
-                            },
-                        ]
-                    },
-                ]
-            },
-            {
-                descriptor: "div",
-                classes: "respond__action-links, clearfix-block",
-            }
-        ],
-    }];*/
-
     function Node(element) {
         var self = this;
-        this.node = document.createElement(element.descriptor.toString());
-
+        if (element.descriptor === "use" || element.descriptor === "svg") {
+            this.node = document.createElementNS("http://www.w3.org/2000/svg", element.descriptor.toString());
+        } else {
+            this.node = document.createElement(element.descriptor.toString());
+        }
         this.createNode = function () {
             if (element.classes) {
                 element.classes.split(", ").forEach(function (item) {
                     self.node.classList.add(item.toString());
                 });
             }
-            if (element.text){
-               self.node.textContent = element.text;
+            if (element.attributes) {
+                for (var key in element.attributes) {
+                    if (element.descriptor === "use" || element.descriptor === "svg") {
+                        self.node.setAttributeNS("http://www.w3.org/1999/xlink", key, element.attributes[key]);
+                    } else {
+                        self.node.setAttribute(key, element.attributes[key]);
+                    }
+                }
+            }
+            if (element.text) {
+                self.node.textContent = element.text;
             }
         };
 
@@ -87,11 +33,13 @@
         this.createNode();
     }
 
-    function Tree(nodeTree, parent = document.createElement("div")) {
+    function Tree(nodeTree, parent = null) {
         this.parent = parent;
         this.node = new Node(nodeTree);
         this.node = this.node.getNode();
-        this.parent.appendChild(this.node);
+        if (this.parent) {
+            this.parent.appendChild(this.node);
+        }
         this.children = nodeTree.children;
 
         this.run = function () {
@@ -100,7 +48,7 @@
             }
         };
 
-        this.getTree = function () {
+        this.getTreeHTML = function () {
             console.log(this.node);
             return this.node;
         };
@@ -113,78 +61,97 @@
         buttonSubmit = formRespond.querySelector(".submitRespondBtn-js");
 
     //buttonSubmit.addEventListener("click", parseForm);
-    formRespond.addEventListener("submit", parseForm);
+    formRespond.addEventListener("submit", addRespond);
 
-    function parseForm(e) {
+
+    function addRespond(e) {
         e.preventDefault();
-        //console.log(formRespond.comment);
-        var tree = [],
-             comment = {
-                descriptor: "p",
-                text: formRespond.elements["comment"].value,
-            },
-            advantages = {
-                descriptor: "p",
-                text: formRespond.elements["advantages"].value,
-                children:[
-                    {
-                        descriptor: "span",
-                        text: "Достоинства: ",
-                    },
-                ]
-            },
-            disadvantages = {
-                descriptor: "p",
-                text: formRespond.elements["disadvantages"].value,
-                children: [
-                    {
-                        descriptor: "span",
-                        text: "Недостатки: ",
-                    },
-                ]
-            },
-            userName = {
-                descriptor: "span",
-                classes: "respond__heading__author",
-                text: formRespond.elements["user"].value,
-            },
-            tradeStatus = {
-                    descriptor: "span",
-                    classes: "respond__heading__trade-status",
-            },
-            likes = {
-                descriptor: "span",
-                classes: "respond__heading__likes",
-            },
-            currentDate = {
-                descriptor: "span",
-                classes: "respond__heading__date",
-                text: new Date(),
-            },
-            respondHeadind = {
-                descriptor: "div",
-                classes: "respond__heading, clearfix-block",
-                children: [userName, tradeStatus, likes, currentDate],
-            },
-            respondText = {
-                descriptor: "div",
-                classes: "respond__text",
-                children: [comment, advantages, disadvantages],
-            },
-            respondFooter = {
-                descriptor: "div",
-                classes: "respond__action-links, clearfix-block",
-            },
-            root = {
-                descriptor: "div",
-                classes: "product-responds__respond, respond-js",
-                children: [respondHeadind, respondText, respondFooter],
-            };
-        tree.push(root);
-        var container = document.querySelector(".responds-content_left");
-        var firstRespond = container.querySelector(".respond-js");
-        var respond = new Tree(tree[0]);
-        respond = respond.getTree();
-        container.insertBefore(respond, firstRespond);
+        var container = document.querySelector(".responds-wrapper-js"),
+            firstRespond = container.querySelector(".respond-js"),
+            respond = new Tree(getTreeObject());
+        respond = respond.getTreeHTML();
+        if (firstRespond) {
+            container.insertBefore(respond, firstRespond);
+        } else {
+            container.appendChild(respond);
+        }
+    }
+
+    function createElement(descriptor, classes = "", children = null, text = "", attributes = null) {
+        var element = {
+            descriptor: descriptor,
+        };
+        if (classes) {
+            element.classes = classes;
+        }
+        if (text) {
+            element.text = text;
+        }
+        if (attributes) {
+            element.attributes = attributes;
+        }
+        if (children) {
+            element.children = children;
+        }
+        return element;
+    }
+
+    function createSVG(idSVG, widthSVG, heightSVG) {
+        var use =
+                createElement("use", undefined, undefined, undefined, {"xlink:href": idSVG}),
+            svg =
+                createElement("svg", undefined, [use], undefined, {width: widthSVG, height: heightSVG});
+        return svg;
+    }
+
+    function getTreeObject() {
+        var comment =
+                createElement("p", undefined, undefined, formRespond.comment.value.trim()),
+            userName =
+                createElement("span", "respond__heading__author", undefined, formRespond.user.value.trim()),
+            likes =
+                createElement("span", "respond__heading__likes"),
+            currentDate =
+                createElement("span", "respond__heading__date", undefined, new Date()),
+            respondHeader =
+                createElement("div", "respond__heading, clearfix-block", [userName, likes, currentDate]),
+            respondText =
+                createElement("div", "respond__text", [comment]),
+            complaintSVG =
+                createSVG("#review-complaint", 17, 17),
+            complaintLink =
+                createElement("div", "respond__action-links__complaint-link", [complaintSVG]),
+            replySVG =
+                createSVG("#review-reply-add", 12, 12),
+            linkText =
+                createElement("span", undefined,undefined, "Ответить"),
+            replyLink =
+                createElement("a", "respond__action-links__reply-link, link",[replySVG, linkText],undefined,{"href": "#"}),
+            respondFooter =
+                createElement("div", "respond__action-links, clearfix-block", [replyLink, complaintLink]),
+            root =
+                createElement("div", "product-responds__respond, respond-js", [respondHeader, respondText, respondFooter]),
+            advantages,
+            disadvantages;
+
+        if (formRespond.advantages.value.trim()) {
+            var textElement =
+                    createElement("span", undefined, undefined, "Достоинства: "),
+                container = createElement("p");
+            advantages =
+                createElement("span", undefined, undefined, formRespond.advantages.value.trim());
+            container.children = [textElement, advantages];
+            respondText.children.push(container);
+        }
+        if (formRespond.disadvantages.value.trim()) {
+            var textElement =
+                    createElement("span", undefined, undefined, "Недостатки: "),
+                container = createElement("p");
+            disadvantages =
+                createElement("span", undefined, undefined, formRespond.disadvantages.value.trim());
+            container.children = [textElement, disadvantages];
+            respondText.children.push(container);
+        }
+        return root;
     }
 })();
