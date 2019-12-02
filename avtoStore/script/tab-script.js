c4561d1a86b312595c23092c2f3d9daf5fef0b00
zevsi <vgritsukm@gmail.com>
2019-12-02 19:16:31 +0200

    added media style avto

--- avtoStore/script/tab-script.js
+++ avtoStore/script/tab-script.js
@@ -1,23 +1,25 @@
 (function () {
 
     function tab() {
-       var tabNav = document.querySelectorAll('.tab'),
+        var tabNav = document.querySelectorAll('.tab'),
             tabContent = document.querySelectorAll('.tab-content'),
-           tabName;
+            tabName;
 
-        tabNav.forEach(item=>{
+        tabNav.forEach(item => {
             item.addEventListener('click', selectTabNav)
         });
+
         function selectTabNav() {
-            tabNav.forEach(item =>{
+            tabNav.forEach(item => {
                 item.classList.remove('white-li');
             });
             this.classList.add('white-li');
             tabName = this.getAttribute('data-tab-name');
             selectTabContent(tabName);
         }
+
         function selectTabContent(tabName) {
-            tabContent.forEach(item =>{
+            tabContent.forEach(item => {
                 item.classList.contains(tabName) ? item.classList.add('is-active') : item.classList.remove('is-active');
             });
         }
@@ -28,47 +30,29 @@
     function TochSlider() {
         var self = this;
         this.param = {
-            globBlock:document.querySelector('.glob-bloc-img'),
-            blocClick:document.querySelector('.bloc-click'),
-            elem : document.querySelector('.slider-wrap')
+            globBlock: document.querySelector('.glob-bloc-img'),
+            blocClick: document.querySelector('.bloc-click'),
+            elem: document.querySelector('.slider-wrap')
         };
 
         function elemClick() {
-            self.param.elem.addEventListener('click',function (event) {
-                if (event.target.tagName === 'IMG'){
+            self.param.elem.addEventListener('click', function (event) {
+                if (event.target.tagName === 'IMG') {
                     self.param.blocClick.style.left = event.target.offsetLeft + 'px';
-                    leaving(event.target);
+                    leavingF(event.target);
                 }
             })
         }
-        function leaving(element) {
-           setTimeout(function () {
-               self.param.globBlock.src = './img/'+ element.dataset['slick'];
 
-           }, 300)
+        function leavingF(element) {
+            setTimeout(function () {
+                self.param.globBlock.src = './img/' + element.dataset['slick'];
+
+            }, 300)
         }
 
         elemClick();
     }
+
     TochSlider();
 })();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
