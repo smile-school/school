bbc503376f80a00a66b6db00b04a1dde3e289824
zevsi <vgritsukm@gmail.com>
2019-12-12 17:36:35 +0200

    corect file

--- avtoStore/script/tab-script.js
+++ avtoStore/script/tab-script.js
@@ -54,5 +54,33 @@
         elemClick();
     }
 
+    function blocAcordeon() {
+        var aElement = document.querySelector('.home-mod'),
+            hoverBloc = document.querySelector('.menu-hover-bloc');
+
+        aElement.addEventListener('click',function (e) {
+            e.preventDefault();
+            if(hoverBloc.classList.contains('modef-hover-bloc')){
+                hoverBloc.classList.remove('modef-hover-bloc');
+            }
+            else {
+                hoverBloc.classList.add('modef-hover-bloc');
+            }
+        })
+
+    }
+    blocAcordeon();
+
+    function mediaScrean() {
+        var liTab = document.querySelectorAll('.li-defain'),
+            divTab = document.querySelectorAll('.tab-content');
+
+
+        if (window.matchMedia("(max-width: 740px)").matches){
+            console.log(divTab.getAttribute());
+        }
+    }
+    mediaScrean();
+
     TochSlider();
 })();
